e55fe345c4a0707ab9fd7fb7a4b0b85760506f84
cleitonfranca <cleiton2281@gmail.com>
2014-07-19 18:53:01 -0300

    nova atualizacao

---
+++ src/main/java/br/com/caelum/tarefas/jpa/CarregaTarefa.java
@@ -0,0 +1,20 @@
+package br.com.caelum.tarefas.jpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.caelum.tarefas.models.Tarefas;
+
+public class CarregaTarefa {
+	
+	public static void main(String[] args) {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("tarefas");
+		EntityManager manager = factory.createEntityManager();
+		
+		Tarefas encontrada = manager.find(Tarefas.class, 1L);
+		System.out.println(encontrada.getDescricao());
+		
+		manager.close();
+	}
+}
